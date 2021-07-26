fd3341141e4a679f2bcb9c91414eb7435a336d1c
TsutayaJun <dominopizza.0913@icloud.com>
2021-07-26 12:05:41 +0900

    追加：ゲームマネージャーにタイマーの機能を追加した

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    /// <summary>
+    /// ゲームの状態
+    /// </summary>
+    enum State
+    {
+        CountUp,
+        Stop,
+        Finish,
+    }
+    /// <summary>
+    /// ゲームの状態
+    /// </summary>
+    [SerializeField] State m_state = State.Stop;
+
+    /// <summary>
+    /// 制限時間
+    /// </summary>
+    [SerializeField] float m_limitTime = 30;
+    /// <summary>
+    /// タイマー
+    /// </summary>
+    float m_timer = 0;
+
+    private void Start()
+    {
+        m_timer = m_limitTime;
+    }
+
+    private void Update()
+    {
+        if (m_state == State.CountUp)
+        {
+            m_timer -= Time.deltaTime;
+            Debug.Log((int)m_timer);
+            
+            //タイマーが0になったら終了する
+            if (m_timer <= 0)
+            {
+                CountFinish();
+            }
+        }
+    }
+
+    /// <summary>
+    /// タイマーを開始する
+    /// </summary>
+    public void CountStart()
+    {
+        m_state = State.CountUp;
+    }
+
+    /// <summary>
+    /// タイマーを停止する
+    /// </summary>
+    public void CountStop()
+    {
+        m_state = State.Stop;
+    }
+
+    /// <summary>
+    /// タイマーを終了する
+    /// </summary>
+    void CountFinish()
+    {
+        m_state = State.Finish;
+    }
+}
