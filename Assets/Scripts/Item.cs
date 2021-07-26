0833abde54f8b97a9585985ad03c8b56e4ca494d
Tsuguhiko Okada <tsugu.pc.academy@gmail.com>
2021-07-26 13:23:05 +0900

    スクリプトのコミット

---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// アイテム
+/// </summary>
+public class Item : MonoBehaviour, IAction
+{
+    public void Action()
+    {
+        throw new System.NotImplementedException();
+    }
+}
