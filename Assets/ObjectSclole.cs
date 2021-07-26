9a56ec58999956bfad2f08834587f9f91d0fe28e
takumi425 <fujishima.t425@gmail.com>
2021-07-26 15:38:24 +0900

    スピードを変更できるようにした

--- Assets/ObjectSclole.cs
+++ Assets/ObjectSclole.cs
@@ -5,11 +5,21 @@ using UnityEngine;
 public class ObjectSclole : MonoBehaviour
 {
     [SerializeField] float _scloleSpeed = 2f;
+    MoveSpeedData _speedData;
     // Start is called before the first frame update
-    
+    private void Start()
+    {
+        _speedData = FindObjectOfType<MoveSpeedData>();
+        _scloleSpeed = _speedData.Speed;
+    }
     // Update is called once per frame
     void Update()
     {
         this.gameObject.transform.Translate(Vector2.down * _scloleSpeed);
+    }
+
+    public void ChangeSpeed()
+    {
+        _scloleSpeed = _speedData.Speed;
     }
 }
