e80a1641e30c1787c4b9f33f4f70b7d8f973ff77
TsutayaJun <dominopizza.0913@icloud.com>
2021-07-26 11:22:19 +0900

    追加：アイテムにあったったらログを出力する

---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// アイテム
+/// </summary>
+public class Item : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            Debug.Log("アイテムにあったった");
+        }
+    }
+}
